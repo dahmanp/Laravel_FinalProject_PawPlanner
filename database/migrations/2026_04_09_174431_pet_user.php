e03dceb22ff85a95f2a905644e641fea19292c1a
dahmanp <dahmanp@my.erau.edu>
2026-04-29 00:13:58 -0700

    Cleanup

--- database/migrations/2026_04_09_174431_pet_user.php
+++ database/migrations/2026_04_09_174431_pet_user.php
@@ -12,11 +12,11 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('pet_user', function (Blueprint $table) {
-        $table->id();
-        $table->foreignId('pet_id')->constrained()->onDelete('cascade');
-        $table->foreignId('user_id')->constrained()->onDelete('cascade');
-        $table->timestamps();
-    });
+            $table->id();
+            $table->foreignId('pet_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
     }
 
     /**
